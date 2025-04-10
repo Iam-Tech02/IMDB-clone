b53eab4ef763c82af45df910dd0b77a7c3b29182
I am DEV <157353142+Iam-Tech02@users.noreply.github.com>
2025-04-10 14:46:56 +0530

    Update LoginPage.jsx

--- IMDB-Clone-Frontend/src/screen/LoginPage.jsx
+++ IMDB-Clone-Frontend/src/screen/LoginPage.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     setError(null);
 
     try {
-      const response = await fetch(`${process.env.BACKEND_URI}/auth/login`, {
+      const response = await fetch(`https://imdb-clone-be-production.up.railway.app/api/auth/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
